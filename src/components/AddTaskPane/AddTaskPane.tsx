05cbcdfdf6ca26b10941fc7cc0d302945f620eb3
Kaushik Sarma <kaushik.sarma@ET-C02Y86JKJG5H.local>
2019-08-27 12:34:15 +0530

    State input binding for form

--- src/components/AddTaskPane/AddTaskPane.tsx
+++ src/components/AddTaskPane/AddTaskPane.tsx
@@ -10,9 +10,9 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
     constructor(props) {
         super(props);
         this.state = { 
-            TaskTitle: "",
-            TaskDescription: "",
-            TaskEndDate: null,
+            TaskTitle: '',
+            TaskDescription: '',
+            TaskEndDate: '',
             TaskHasDeadline: false,
             hasalert: false,
             alertheading: '',
@@ -22,12 +22,15 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
     }
 
     handleChange = event => {
+        console.log(this.state);
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked 
-                    : (target.type === 'date' ? target.valueAsDate : target.value);
+        let value = target.type === 'checkbox' ? target.checked : target.value;
+        if (target.type === 'date') {
+            // value = isNaN(Date.parse(value)) ? value : target.valueAsDate;
+        }
         const name = target.name;
         // In case of checkbox we should not prevent default action
-        target.type !== 'checkbox' && event.preventDefault();
+        (target.type !== 'checkbox' && target.type !=='date')&& event.preventDefault();
         this.setState({
             [name]: value
         });
@@ -42,7 +45,7 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
             this.alert('Invalid Date', 'Please enter a valid date of format dd/mm/yyyy', 'failure');
             return false;
         }
-        if (data['TaskHasDeadline'] && data['TaskEndDate'].getTime() < (new Date()).getTime()) {
+        if (data['TaskHasDeadline'] && Date.parse(data['TaskEndDate']) < (new Date()).getTime()) {
             this.alert('Invalid Date', 'Oops! Looks like you set your deadline in the past!', 'failure');
             return false;
         } 
@@ -62,7 +65,7 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
         this.setState({ 
             TaskTitle: "",
             TaskDescription: "",
-            TaskEndDate: null,
+            TaskEndDate: "",
             TaskHasDeadline: false
         });
     }
@@ -72,8 +75,8 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
         console.log(this.state);
         if(this.isValid(this.state)) {
             this.props.addTaskHandler(this.state);
-            // this.resetForm();
-            // event.target.reset();
+            this.resetForm();
+            event.target.reset();
             alert('Todo Added!')
         }
     }
@@ -97,10 +100,11 @@ export default class AddTaskPane extends React.Component<AddTaskProps> {
                 {this.state['hasalert'] && <Alert removeNotification={this.removeNotification} heading={this.state['alertheading']} message={this.state['alertmessage']} type={this.state['alerttype']}/>}
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type="Text" name="TaskTitle" placeholder="Title" onChange={this.handleChange} required></input>
-                    <textarea name="TaskDescription" placeholder="Add a description" onChange={this.handleChange}></textarea>
+                    <input type="Text" name="TaskTitle" placeholder="Title" onChange={this.handleChange} value={this.state['TaskTitle']} required></input>
+                    <textarea name="TaskDescription" placeholder="Add a description" onChange={this.handleChange} value={this.state['TaskDescription']}></textarea>
                     <input type="checkbox" name="TaskHasDeadline" onChange={this.handleChange} checked={this.state['TaskHasDeadline']}></input> Add Deadline?
-                    {hasDeadline && <input type="date" name="TaskEndDate" placeholder='Deadline' onChange={this.handleChange} min='$todaymin' required={hasDeadline}></input>}
+                    {hasDeadline && 
+                    <input type="date" name="TaskEndDate" placeholder='Deadline' onChange={this.handleChange} value={this.state['TaskEndDate']} required={hasDeadline}></input>}
                     <button type="submit">Add</button>
                 </form>
             </div>
--- src/index.tsx
+++ src/index.tsx
@@ -36,7 +36,7 @@ const TodoReducer = (state:AppState = new AppState(new TasksList(0,
             const newstate = new AppState(new TasksList(state.todos.getCurrentTaskID(), ...state.todos.getTasks(), new Task({
                                     "title": action.data["TaskTitle"],
                                     "description": action.data["TaskDescription"],
-                                    "enddate": action.data["TaskEndDate"]
+                                    "enddate": new Date(action.data["TaskEndDate"])
                                 })
                             ), state.visibilityFilter);
             return newstate;
